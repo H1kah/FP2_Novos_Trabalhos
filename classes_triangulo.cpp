06fc1dce7f5d466c0b8885d97e82a43404bad899
H1karu <124624556+H1kah@users.noreply.github.com>
2023-05-01 14:17:54 -0300

    Update classes_triangulo.cpp

--- classes_triangulo.cpp
+++ classes_triangulo.cpp
@@ -1,12 +1,11 @@
 #include <iostream>
 #include <cmath>
-using namespace std;
 
 class Triangulo {
     private: 
         double a, b, c;
     public:
-        Triangulo (double lado1, double lado2, double lado3) {7
+        Triangulo (double lado1, double lado2, double lado3) {
             a = lado1;
             b = lado2;
             c = lado3;
@@ -14,18 +13,14 @@ class Triangulo {
 
     double calcularArea () {
         double p = (a+b+c) / 2;
-        double area = std: :sqrt(p * (p-a) * (p-b) (p-c));
+        double area = std::sqrt(p * (p-a) * (p-b)*(p-c));
         return area;
     }
     double getlado1() { return a; }
     double getlado2() { return b; }
     double getlado3() { return c; }
-}
+};
 int main () {
-    
-    //repetição de cria
-    char continuar = 's';
-    while (continuar == 's' || continuar == 'S') {
     
     double a, b, c;
     std:: cout <<"Digite as medidas dos lados dos do triângulo X (separe por espaço): ";
@@ -36,28 +31,23 @@ int main () {
     std:: cin >> a >> b >> c;
     Triangulo Y (a, b, c);
     
-    double AreaX = X.calculaArea();
-    double AreaY = Y.calculaArea();
+    double AreaX = X.calcularArea();
+    double AreaY = Y.calcularArea();
     
-    std:: cout << "Área do triângulo X: " << areaX << std::endl;
-    std:: cout << "Área do triângulo Y: " << areaY << std ::endl;
+    std:: cout << "Área do triângulo X: " << AreaX << std::endl;
+    std:: cout << "Área do triângulo Y: " << AreaY << std::endl;
     
-    if (areaX > areaY) {
-        std: :cout << "Triângilo X tem o maior área." <<  std::endl;
+    if (AreaX > AreaY) {
+        std::cout << "Triângilo X tem a maior área." << std::endl;
     }
-    else if (areaY > areaX) {
+    else if (AreaY > AreaX) {
+        std::cout << "Triângilo Y tem a maior área." << std::endl;
     }
-        std: :cout << "Triângilo Y tem o maior área." <<  std::endl;
-    else if (areaY = areaX) {
-        std: :cout << "Ambos possuem a mesma área." <<  std::endl;
+    else if (AreaY = AreaX) {
+        std::cout << "Ambos possuem a mesma área." << std::endl;
     }
     else {
-        std: :cout << "erro" <<  std::endl;
+        std::cout << "erro" <<  std::endl;
 
     }
-    //tive que colocar uma repetição pra ficar bom mesmo
-    cout << "Deseja continuar (s/n)?: ";
-    cin >> continuar;
-    return 0;
-}
 }
