72f3365973d7fff0da14bb630747ba0a423cd91f
H1karu <124624556+H1kah@users.noreply.github.com>
2023-05-01 14:42:24 -0300

    Update classes_triangulo.cpp

--- classes_triangulo.cpp
+++ classes_triangulo.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <cmath>
-
+//A classe triângulo está sendo criada primeiro para que possa ser utilizada depois no int main, apenas citando a palavra chave. 
+//No caso, está sendo utilizado o private para que esteja, como a palavra diz, privada. Ou seja, todo o conteúdo estará privado apenas para essa parte,
+// não será assossiada com o resto. Já o public, toda citação entre ela poderá ser utilizada de forma livre. Esses códigos são utilizados para que haja uma organização
+//e uma forma de facilitar o resto do trabalho, pegando o que é preciso apenas no momento desejado.
 class Triangulo {
     private: 
         double a, b, c;
@@ -20,6 +23,10 @@ class Triangulo {
     double getlado2() { return b; }
     double getlado3() { return c; }
 };
+// A partir daqui, será feito as assossições, comentários que aparecerão na tela, etc. Toda resposta que foi feita a partir da questão na tela será direcionada
+//para substituir uma parte do código. Logo, com um simples comando (resultado do public), calculará os valores dados usando a fórmula criada anteriormente. 
+//Por fim, a resposta será dada. De acordo com os valores finais (ou seja, se for maior ou menor), mostrará na tela qual é a diferença entre eles ou se ambos os valores
+//são iguais.
 int main () {
     
     double a, b, c;
